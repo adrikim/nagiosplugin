3a3f647769ff3c091f187cf06b9a5def050a9dcc
Adrian Kim <adrikim@umich.edu>
2015-10-14 14:44:52 -0400

    Add status code reporting

--- plugin.py
+++ plugin.py
@@ -17,12 +17,11 @@ It should report most of the data as perfdata. Definitely at least:
 # http://es-cluster.engin.umich.edu:9200/_stats?pretty=true
 url = 'http://es-cluster.engin.umich.edu:9200/_cluster/health'
 info = json.loads(urllib2.urlopen(url).read())
-#print json.dumps(info)
 
 # Get overall status
-status = ("Status OK" if info["status"] == "green"
-          else "Status WARNING" if info["status"] == "yellow"
-          else "Status CRITICAL") # critical is either that or unresponsive
+status = (0 if info["status"] == "green"
+          else 1 if info["status"] == "yellow"
+          else 2) # critical is either that or unresponsive
 
 # Get perfdata #
 # Node data
@@ -35,9 +34,13 @@ n_total_shards = info["active_shards"] # Should be the same as total # shards
 n_pending_actions = info["number_of_pending_tasks"]
 
 # Print out to stdout in Icinga plugin format
-print status, "|",
+print "Status", ("OK" if status == 0
+                 else "WARNING" if status == 1
+                 else "CRITICAL"), "|",
 print "n_nodes=" + str(n_nodes),
 print "n_datanodes=" + str(n_datanodes),
 print "n_primary_shards=" + str(n_primary_shards),
 print "n_total_shards=" + str(n_total_shards),
 print "n_pending_actions=" + str(n_pending_actions)
+
+sys.exit(status)
